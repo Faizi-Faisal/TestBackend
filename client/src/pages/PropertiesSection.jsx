5a9b87016613ca889dca6b1fbcca6b0b31fc15f8
Faisal A <153807433+Faizi-Faisal@users.noreply.github.com>
2024-08-08 11:57:39 +0530

    Update PropertiesSection.jsx

--- client/src/pages/PropertiesSection.jsx
+++ client/src/pages/PropertiesSection.jsx
@@ -31,7 +31,7 @@ const PropertiesSection = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://ser-dep.vercel.app/api/properties`);
+        const response = await axios.get(`https://ser-dep.vercel.app/api/properties/paginated?page=${page}&limit=${limit}`);
         setProperties(prevProperties => [...prevProperties, ...response.data.properties]);
         setHasMore(response.data.properties.length > 0);
         setLoading(false);
